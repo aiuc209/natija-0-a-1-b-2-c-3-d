999f17123a785debc227668c49cbaed70cb68522
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:28:03 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Natija 0 A 1 B 2 C 3 D
+Topic: Natija: ((0, "a"), (1, "b"), (2, "c"), (3, "d"))
+"""
+
+def main():
+    print("Welcome to Natija 0 A 1 B 2 C 3 D!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
